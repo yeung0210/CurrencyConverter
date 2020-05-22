49bcd9421bf826cc3d849dbbdf117d218dec3e60
Sammi <sammiyc0210@gmail.com>
2020-05-23 01:48:53 +0800

    Home Screen: Closing the Keyboard

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, KeyboardAvoidingView } from 'react-native';
+import { View, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Styles from './styles';
 
 const Container = ({ children }) => (
     <KeyboardAvoidingView
         behavior="padding"
         style={{ flex: 1 }}>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
         <View style={Styles.container}>
             {children}
         </View>
+    </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
     
 );
